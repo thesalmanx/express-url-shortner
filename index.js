096d455bde3de4a9cb1d2c992175403045bcbc19
Muhammad Salman <111143401+thesalmanx@users.noreply.github.com>
2024-11-27 02:55:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connect");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+require("dotenv").config();
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB(process.env.MONGODB).then(() =>
+  console.log("Mongodb connected")
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () =>
+  console.log(`Server Started at : http://localhost:${PORT}`)
+);
